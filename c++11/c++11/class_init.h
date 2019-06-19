28f9aa0f6b992216bc7be6ae08b8b3d918f893ed
伍林峰 <1191968876@qq.com>
2019-06-19 22:40:32 +0800

    test4

--- c++11/c++11/class_init.h
+++ c++11/c++11/class_init.h
@@ -8,7 +8,7 @@ using namespace std;
 class Init_Test1
 {
 public:
-	Init_Test1(int b = 2) :m_b(b), m_c(b){ m_c = 3; m_d = 3; };
+	Init_Test1(int b = 2) :m_b(b), m_c(b){ m_c = 3; m_d = 3; };1111111
 
 	//cout 1233 主要看看3个地方初始化的优先情况
 	void display(){ std::cout << m_a << m_b << m_c << m_d << endl; }
